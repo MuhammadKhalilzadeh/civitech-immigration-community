31a07b428d9ba5339073e205ec82eb7d85478de2
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-01-20 11:25:36 +0330

    Sign in form added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./authentication/signup";
+import Signin from "./authentication/signin";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Signin />} />
       </Routes>
     </Router>
   );
